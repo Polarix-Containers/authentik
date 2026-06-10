a3fd53592a0a5fb831dd41477e8bc2ab963c0225
Tommy <contact@tommytran.io>
2026-06-10 07:47:34 +0000

    Authentik v2026.5.2

--- scripts/generate_semver.py
+++
@@ -1,8 +0,0 @@
-# Dummy python script invoked by Makefile
-# to generate a version number
-
-# Upstream's code does not work in our workflow for whatever reason,
-# so we provide our own
-
-import os
-print(os.environ['TAG'])
